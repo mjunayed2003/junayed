559a83494b7df680c4da8ce3a9ca8c08846f3a65
M-JUNAYED <junayedislammd8@gmail.com>
2025-10-11 20:47:43 +0600

    png

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "junayed",
+  title: "mdjunayed",
   description: "Md.Junayed Portfolio",
 };
 
