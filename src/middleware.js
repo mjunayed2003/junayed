992634074b3b44002c08fcc5b8f405ed83bbcbd9
mdjunayed2003 <junayedislammd8@gmail.com>
2025-10-23 20:36:53 +0600

    dashboard open

--- src/middleware.js
+++ src/middleware.js
@@ -1,40 +1,50 @@
+// import { NextResponse } from "next/server";
+// import { VerifyToken } from "@/utility/JWTTokenHelper";
+
+// export async function middleware(req, res) {
+//   // Security check for Frontend
+//   if (req.nextUrl.pathname.startsWith("/dashboard")) {
+//     try {
+//       let token = req.cookies.get("token");
+
+//       let payload = await VerifyToken(token["value"]);
+
+//       const requestHeader = new Headers(req.headers);
+//       requestHeader.set("email", payload["email"]);
+//       requestHeader.set("id", payload["id"]);
+//       return NextResponse.next({
+//         request: { headers: requestHeader },
+//       });
+//     } catch (e) {
+//       return NextResponse.redirect(new URL("/login", req.url));
+//     }
+//   }
+
+//   // Security check for API endpoint
+//   if (req.nextUrl.pathname.startsWith("/api/dashboard")) {
+//     try {
+//       let token = req.cookies.get("token");
+
+//       let payload = await VerifyToken(token["value"]);
+
+//       const requestHeader = new Headers(req.headers);
+//       requestHeader.set("email", payload["email"]);
+//       requestHeader.set("id", payload["id"]);
+//       return NextResponse.next({
+//         request: { headers: requestHeader },
+//       });
+//     } catch (e) {
+//       return NextResponse.json({ status: false, data: "Unauthorized!" });
+//     }
+//   }
+// }
+
+
+
+
 import { NextResponse } from "next/server";
-import { VerifyToken } from "@/utility/JWTTokenHelper";
 
-export async function middleware(req, res) {
-  // Security check for Frontend
-  if (req.nextUrl.pathname.startsWith("/dashboard")) {
-    try {
-      let token = req.cookies.get("token");
-
-      let payload = await VerifyToken(token["value"]);
-
-      const requestHeader = new Headers(req.headers);
-      requestHeader.set("email", payload["email"]);
-      requestHeader.set("id", payload["id"]);
-      return NextResponse.next({
-        request: { headers: requestHeader },
-      });
-    } catch (e) {
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
-  }
-
-  // Security check for API endpoint
-  if (req.nextUrl.pathname.startsWith("/api/dashboard")) {
-    try {
-      let token = req.cookies.get("token");
-
-      let payload = await VerifyToken(token["value"]);
-
-      const requestHeader = new Headers(req.headers);
-      requestHeader.set("email", payload["email"]);
-      requestHeader.set("id", payload["id"]);
-      return NextResponse.next({
-        request: { headers: requestHeader },
-      });
-    } catch (e) {
-      return NextResponse.json({ status: false, data: "Unauthorized!" });
-    }
-  }
+// Middleware can not verify
+export async function middleware(req) {
+  return NextResponse.next();
 }
